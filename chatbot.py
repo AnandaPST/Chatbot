259b379d82526eb951453cafdebb56988dce2b7b
AnandaPST <103588733+AnandaPST@users.noreply.github.com>
2024-07-19 14:33:24 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import ollama
+import subprocess
+import sys
+
+PACKAGE = ["crewai", "faiss-cpu", "lang", "ollama"]
+
+def install(package):
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing {package}: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred while installing {package}: {e}")
+
+for i in PACKAGE :
+    install(i)
 
 st.title("💬 Johannes")
 
